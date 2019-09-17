423a1ad298393ce809e75d6f0c395f04222a25e6
Tatiana Smolin <tatismolin@gmail.com>
2019-09-17 23:09:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,7 +14,13 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.max
+  max = array[0]
+  for item in array
+    if item > max
+      max = item 
+    end
+  end
+  max
 end
 
 def find_min_value(array)
