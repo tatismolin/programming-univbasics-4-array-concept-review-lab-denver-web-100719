8e5b4b5c18917ccc3846b5c117d1e5ae020621cf
Tatiana Smolin <tatismolin@gmail.com>
2019-09-17 23:10:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,12 +17,18 @@ def find_max_value(array)
   max = array[0]
   for item in array
     if item > max
-      max = item 
+      max = item
     end
   end
   max
 end
 
 def find_min_value(array)
-  array.min
+  min = array[0]
+  for item in array
+    if item > min
+      min = item
+    end
+  end
+  min
 end
